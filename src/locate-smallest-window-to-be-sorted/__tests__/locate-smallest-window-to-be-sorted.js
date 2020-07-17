40089bb0382188aff64a20bdee8a539b273c745a
volodymyrmordas <vmordas@alteryx.com>
2020-07-17 11:10:07 +0300

    locate-smallest-window-to-be-sorted

--- src/locate-smallest-window-to-be-sorted/__tests__/locate-smallest-window-to-be-sorted.js
+++ src/locate-smallest-window-to-be-sorted/__tests__/locate-smallest-window-to-be-sorted.js
@@ -6,4 +6,22 @@ describe(`should test`, () => {
     const expected = [1, 3];
     expect(getSmallestWindowToBeSorted(data)).toEqual(expected);
   });
+  
+  it(`sorted should be success `, () => {
+    const data = [3, 5, 6, 7, 9];
+    const expected = [0, 4];
+    expect(getSmallestWindowToBeSorted(data)).toEqual(expected);
+  });
+  
+  it(`unsorted should be success `, () => {
+    const data = [9, 7, 6, 5, 1];
+    const expected = [0, 4];
+    expect(getSmallestWindowToBeSorted(data)).toEqual(expected);
+  });
+  
+  it(`same should be success `, () => {
+    const data = [0, 9, 9, 7, 5, 6, 5, 10];
+    const expected = [1, 6];
+    expect(getSmallestWindowToBeSorted(data)).toEqual(expected);
+  });
 });
